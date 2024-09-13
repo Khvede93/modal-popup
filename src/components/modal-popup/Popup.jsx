03e5d10b2a09bec87412c69024bb76fd3b0819e7
Khvede93 <gio.khvede93@gmail.com>
2024-09-13 18:47:17 +0200

    temp stucture with default values

--- src/components/modal-popup/Popup.jsx
+++ src/components/modal-popup/Popup.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
-export const Popup = () => {
-  return <div className='popup-container'>Popup</div>;
+export const Popup = ({ id, header, body, footer }) => {
+  return (
+    <div id={id || 'Popup'} className='popup-container'>
+      <div className='content'>
+        <div className='header'>
+          <span className='close-popup-icon'>&times;</span>
+          <h2>{header ? header : 'Default Header'}</h2>
+        </div>
+        <div className='body'>
+          {body ? body : <div>Default Body Content</div>}
+        </div>
+        <div className='footer'>
+          {footer ? footer : <h2>Default Footer</h2>}
+        </div>
+      </div>
+    </div>
+  );
 };
